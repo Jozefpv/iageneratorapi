ce12853b5ab72d7457d6d10e85dfdfa96c635b24
jozef <jozefpv99@gmail.com>
2024-12-21 23:57:33 +0000

    añadimos correccions de la sesion 3

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -1,7 +1,7 @@
 const corsOptions = {
     origin: ['https://jozefpv.github.io', 'http://localhost:4200'],
-    methods: 'GET,POST',
-    allowedHeaders: 'Content-Type,Authorization',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization,Set-Cookie',
     credentials: true
 };
 
