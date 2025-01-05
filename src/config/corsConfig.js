260fe62cb07272d098b4daf0b7414577c785bda3
jozef <jozefpv99@gmail.com>
2025-01-05 16:57:11 +0000

    impelmentamos el dominio nuevo

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ['https://jozefpv.github.io', 'http://localhost:4200'],
+    origin: ['https://jozefpv.github.io', 'http://localhost:4200', 'https://www.pintaloo.com', 'http://www.pintaloo.com'],
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization,Set-Cookie',
     credentials: true
@@ -7,7 +7,7 @@ const corsOptions = {
 
 const ioCorsOptions = {
     cors: {
-        origin: ['https://jozefpv.github.io', 'http://localhost:4200'],
+        origin: ['https://jozefpv.github.io', 'http://localhost:4200', 'https://www.pintaloo.com', 'http://www.pintaloo.com'],
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type', 'Authorization'],
     },
